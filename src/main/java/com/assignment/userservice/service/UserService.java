5f173e21a072c1b9987cfacfc9ad88e1ef1b503a
Jihun <jm3420@naver.com>
2025-07-29 16:16:06 +0900

    feat: UserService에 유저 정보 조회, 수정, 삭제 서비스 구현

--- src/main/java/com/assignment/userservice/service/UserService.java
+++ src/main/java/com/assignment/userservice/service/UserService.java
@@ -2,9 +2,15 @@ package com.assignment.userservice.service;
 
 import com.assignment.userservice.dto.UserResponse;
 import com.assignment.userservice.dto.UserSignupRequest;
+import org.springframework.data.domain.Page;
 
 public interface UserService {
 
     UserResponse register(UserSignupRequest request);
 
+    Page<UserResponse> searchUsers(int page, int size);
+
+    UserResponse modifyUsersPasswordAndAddress(String userId, String newPassword, String newAddress);
+
+    void deleteUsers(String userId);
 }
--- src/main/java/com/assignment/userservice/service/UserServiceImpl.java
+++ src/main/java/com/assignment/userservice/service/UserServiceImpl.java
@@ -5,8 +5,12 @@ import com.assignment.userservice.dto.UserSignupRequest;
 import com.assignment.userservice.entity.Users;
 import com.assignment.userservice.enums.UserRole;
 import com.assignment.userservice.exception.DuplicationUserException;
+import com.assignment.userservice.exception.UserNotFoundException;
 import com.assignment.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,7 +42,35 @@ public class UserServiceImpl implements UserService{
             throw new DuplicationUserException("이미 존재하는 주민등록번호입니다.");
         }
     }
-    
+
+    @Transactional(readOnly = true)
+    public Page<UserResponse> searchUsers(int page, int size) {
+        Pageable pageable = Pageable.ofSize(size).withPage(page);
+        Page<Users> usersPage = userRepository.findAll(pageable);
+        return usersPage.map(UserResponse::of);
+    }
+
+    @Transactional
+    public UserResponse modifyUsersPasswordAndAddress(String userId, String newPassword, String newAddress) {
+        Users user = userRepository.findByUserId(userId).orElseThrow(() ->
+                new UserNotFoundException(String.format("사용자 ID '%s'를 찾을 수 없습니다.", userId)
+        ));
+
+        user.updatePassword(passwordEncoder.encode(newPassword));
+        user.updateAddress(newAddress);
+
+        return UserResponse.of(user);
+    }
+
+    @Transactional
+    public void deleteUsers(String userId) {
+        Users user = userRepository.findByUserId(userId).orElseThrow(() ->
+                new UserNotFoundException(String.format("사용자 ID '%s'를 찾을 수 없습니다.", userId)
+        ));
+
+        user.delete();
+    }
+
     private Users createUsers(UserSignupRequest request) {
         return Users.builder()
                 .userId(request.getUserId())
