998e51ded3201a2525135a54cf8dc4d7582d412d
Jihun <jm3420@naver.com>
2025-07-29 16:14:32 +0900

    feat: UserRepository에 페이지네이션 인터페이스 추가

--- src/main/java/com/assignment/userservice/repository/UserRepository.java
+++ src/main/java/com/assignment/userservice/repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.assignment.userservice.repository;
 
 import com.assignment.userservice.entity.Users;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<Users,Long> {
+public interface UserRepository extends CrudRepository<Users,Long>, PagingAndSortingRepository<Users, Long> {
 
     Optional<Users> findByUserId(String userId);
 
