8ee755bc72cc0095f60b8748437f73c261eb257c
Jihun <jm3420@naver.com>
2025-08-02 22:54:32 +0900

    feat: securityconfig 설정 수정

--- src/main/java/com/assignment/userservice/config/SecurityConfig.java
+++ src/main/java/com/assignment/userservice/config/SecurityConfig.java
@@ -1,28 +1,20 @@
 package com.assignment.userservice.config;
 
 import com.assignment.userservice.enums.UserRole;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -39,7 +31,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)// CSRF 보호 비활성화 (REST API의 경우)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-                .httpBasic(basic -> basic.realmName("Basic Auth"))
+
                 .authorizeHttpRequests(authorize -> authorize
                         // 공개 엔드포인트 설정
                         .requestMatchers("/api/public/**",
@@ -48,22 +40,10 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/**").hasRole(UserRole.ADMIN.name())
                         // 그 외 모든 요청은 인증 필요
                         .anyRequest().authenticated()
-                );
 
-        //로깅설정
-        http.addFilterBefore(new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain filterChain) throws ServletException, IOException {
-                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-                if (auth != null) {
-                    log.debug("Current user: {}", auth.getName());
-                    log.debug("Authorities: {}", auth.getAuthorities());
-                }
-                filterChain.doFilter(request, response);
-                }
-            }, UsernamePasswordAuthenticationFilter.class);
+                )
+                .httpBasic(Customizer.withDefaults());
+
 
         return http.build();
     }
