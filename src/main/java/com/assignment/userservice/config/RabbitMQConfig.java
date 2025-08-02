0b4c559392888b2f91e5f8ac515244fa8eef437c
Jihun <jm3420@naver.com>
2025-08-02 20:43:22 +0900

    feat: rabbitmq 직렬화 컨버터 추가, 메세지가 실패시 반복처리되지 않도록 수정

--- src/main/java/com/assignment/userservice/config/RabbitMQConfig.java
+++ src/main/java/com/assignment/userservice/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.assignment.userservice.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +19,28 @@ public class RabbitMQConfig {
 
     @Value("${rabbitmq.routing.kakao}")
     private String routingKey;
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter); // JSON 직렬화 지원 추가
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setDefaultRequeueRejected(false); // 메시지가 반복적으로 처리되지 않도록 설정
+        factory.setMessageConverter(messageConverter());
+        return factory;
+    }
 
     @Bean
     public Queue messageQueue() {
@@ -53,7 +79,7 @@ public class RabbitMQConfig {
         return BindingBuilder
                 .bind(deadLetterQueue)
                 .to(deadLetterExchange)
-                .with(routingKey);
+                .with(routingKey + ".dlq");
     }
 }
 
